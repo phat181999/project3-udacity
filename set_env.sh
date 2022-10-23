b11fda8b36868bc3e459429c392044c14b4c82ab
phat181999 <phat181999@gmail.com>
2022-10-23 20:14:19 +0700

    update code

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=dbproject3
-export POSTGRES_PASSWORD=dbproject3
-export POSTGRES_HOST=dbproject3.cizgxfork1ex.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=dbproject3
-export AWS_BUCKET=test-project3-dev
+export POSTGRES_USERNAME=project3udagram
+export POSTGRES_PASSWORD=project3udagram
+export POSTGRES_HOST=project3udagram.c4yfkyqlfjl6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project3udagram
+export AWS_BUCKET=project-3-s3
 export AWS_REGION=us-east-1a
 export AWS_PROFILE=default
 export JWT_SECRET=testing
