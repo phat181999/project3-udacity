0d327c3abc8756914ef8f134426764f3d7209051
phat181999 <phat181999@gmail.com>
2022-10-24 15:14:37 +0700

    update kubectl eviroment

--- udagram-api-feed/www/config/config.js
+++ udagram-api-feed/www/config/config.js
@@ -10,8 +10,8 @@ exports.config = {
     aws_region: process.env.AWS_REGION,
     aws_profile: process.env.AWS_PROFILE,
     aws_media_bucket: process.env.AWS_BUCKET,
-    aws_access_key_id: "AKIASTNO3K4OKCYWVBXD",
-    aws_secret_access_key: "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",
+    aws_access_key_id: "AKIASTNO3K4OLHU5AZP3",
+    aws_secret_access_key: "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",
     url: process.env.URL,
     jwt: {
         secret: process.env.JWT_SECRET,
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "abaa6f629ef0c4a58ba418375925c6b1-1904321587.us-east-1.elb.amazonaws.com",
 };
 
 /*
