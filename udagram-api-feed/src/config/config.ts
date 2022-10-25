b9ed0e8b56251d2420937934382cb8b4627e4a7e
phat181999 <phat181999@gmail.com>
2022-10-26 00:40:12 +0700

    fix

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -7,8 +7,8 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  aws_access_key_id: "AKIASTNO3K4OLHU5AZP3",
-  aws_secret_access_key: "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",
+  aws_access_key_id: "AKIASTNO3K4OONASVBZQ",
+  aws_secret_access_key: "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",
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
