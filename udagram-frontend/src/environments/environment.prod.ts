20a567fbbbc08c4036375af68720d01731db94d7
phat181999 <phat181999@gmail.com>
2022-10-25 21:14:09 +0700

    update code

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   apiHost:
-    "http://abaa6f629ef0c4a58ba418375925c6b1-1904321587.us-east-1.elb.amazonaws.com:8080/api/v0",
+    "http://a2dee25f804a346beb67860545073230-800951941.us-east-1.elb.amazonaws.com:8100/api/v0",
 };
 
 /*
